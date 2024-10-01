adac8abca1af4d980d318eb56714bcea1c61b0cf
musteey <mustaphasunusi5@gmail.com>
2024-10-01 20:46:57 +0100

    Created boiler plate

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+        <header>
+            <section className='font-semibold text-red-800'>
+                inter
+            </section>
+            <nav>
+
+            </nav>
+        </header>
+    </>
+  )
+}
+
+export default NavBar
